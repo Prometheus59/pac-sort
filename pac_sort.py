b6818d46965f3fbb3af8711f8c1663056f2bf473
Ryan Karumanchery <ryankarumanchery@gmail.com>
2020-07-19 16:28:25 -0400

    Added a prediction/classification algorithm

--- pac_sort.py
+++ pac_sort.py
@@ -14,16 +14,25 @@ def get_dist(tuple):
     return tuple[1]
 
 
-def get_neighbours(train, test_row, neighbour_count):
+def get_neighbours(train_set, test_row, neighbour_count):
+    # Get distances of each rows and sort
     dists = list()
-    for train_row in train:
-        dist = euclidean_dist(test_row, train_row)
-        dists.append((train_row, dist))
+    for row in train_set:
+        dist = euclidean_dist(test_row, row)
+        dists.append((row, dist))
     dists.sort(key=get_dist)
+
+    # Add distances to neighbour list
     neighbours = list()
     for i in range(neighbour_count):
         neighbours.append(dists[i][0])
     return neighbours
+
+
+def classify(training_set, test_row, neighbour_count):
+    neighbours = get_neighbours(training_set, test_row, neighbour_count)
+    results = [row[-1] for row in neighbours]  # Get last element of each row
+    return max(set(results), key=results.count)
 
 
 test_dataset = [[2.7810836, 2.550537003, 0],
@@ -37,6 +46,6 @@ test_dataset = [[2.7810836, 2.550537003, 0],
                 [8.675418651, -0.242068655, 1],
                 [7.673756466, 3.508563011, 1]]
 
-neighbours = get_neighbours(test_dataset, test_dataset[0], 3)
-for neighbour in neighbours:
-    print(neighbour)
+
+prediction = classify(test_dataset, test_dataset[0], 3)
+print('Expected %d, Got %d.' % (test_dataset[0][-1], prediction))
